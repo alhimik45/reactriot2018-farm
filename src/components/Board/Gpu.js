d78f46b054f07606183bc4ff3af1198ba828be06
idib <id.ib.mail@gmail.com>
2018-09-15 13:05:47 +0300

    add select round

--- src/components/Board/Gpu.js
+++ src/components/Board/Gpu.js
@@ -1,5 +1,14 @@
 import React from 'react'
 import gpu from '../../img/gpunvidia.png'
+import './Gpu.css'
 
 export const Gpu = props =>
-  <img src={gpu} alt="gpu"/>
+    <div>
+        <img src={gpu} alt="gpu"/>
+
+
+        <div className="BitCointRound"></div>
+        <div className="EthereumRound"></div>
+        <div className="Round3"></div>
+        <div className="Round4"></div>
+    </div>
