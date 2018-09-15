44c0b117a03ef83ceba4f70301d240980457b114
alhimik45 <mail@alhimik.me>
2018-09-15 07:04:53 +0500

    fixes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import React from 'react';
 import './App.css';
-import { Col, Grid, Row } from 'react-bootstrap'
+import { Col, Row } from 'react-bootstrap'
 
 import Board from './components/Board'
 import RightPanel from './components/RightPanel'
 
 const App = () =>
   <div className="littleMargin">
-      <Row>
-        <Col md={8}>
-          <Board/>
-        </Col>
-        <Col md={4}>
-          <RightPanel
-            dollar="15"
-          />
-        </Col>
-      </Row>
+    <Row>
+      <Col md={8}>
+        <Board/>
+      </Col>
+      <Col md={4}>
+        <RightPanel
+          dollar="15"
+        />
+      </Col>
+    </Row>
   </div>;
 
 export default App;
