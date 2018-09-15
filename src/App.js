87ab3eb40cf8626a79fa7af6a674c4549a405910
RightJelkin <romgalkin23@gmail.com>
2018-09-15 15:30:34 +0700

    Trading component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, {Component} from 'react';
-import ReactDOM from 'react-dom';
 import './App.css';
 import {Col, Row} from 'react-bootstrap'
 import ReactModal from 'react-modal'
 import RightPanel from './components/RightPanel'
 import Welcome from './components/Welcome'
+import Trading from './components/Trading'
 import { BoardConnected } from './connected-components'
 
 ReactModal.setAppElement('#root');
@@ -17,6 +17,9 @@ class App extends Component {
             <div>
                 {/*<button onClick={this.handleOpenModal}>Trigger Modal</button>*/}
                 <Welcome/>
+            </div>
+            <div>
+                <Trading/>
             </div>
             <div className="littleMargin">
                 <Row>
